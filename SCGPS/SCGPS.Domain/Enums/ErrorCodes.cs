e199564c5433c954edf5ba8c54fc7b9cc0fbbf59
Máté Stancz <stancz.mate@gmail.com>
2022-09-02 12:25:56 +0200

    Hiba kódok

--- SCGPS/SCGPS.Domain/Enums/ErrorCodes.cs
+++ SCGPS/SCGPS.Domain/Enums/ErrorCodes.cs
@@ -9,8 +9,17 @@ namespace SCGPS.Domain.Enums
 {
     public enum ErrorCodes
     {
+        #region General: 0000-0099
+        [Description("Ismeretlen hiba")]
+        UncategorizedError = 0000,
+        [Description("Validációs hiba")]
+        ValidationError = 0001,
+        //[Description("")]
+
+        #endregion
+
         #region Service (General): 0100-0199
-        [Description("A keresett entitás nem található")]
+        [Description("A keresett adat nem található")]
         ServiceGeneralEntityNotFound = 0100,
         [Description("Az adott entitásnak nincs azonosítója")]
         ServiceGeneralNoId = 0101,
@@ -21,8 +30,15 @@ namespace SCGPS.Domain.Enums
         #endregion
 
         #region ReviewService: 0300-0399
+        [Description("Az értékelt film nem található")]
         ReviewServiceMovieNotFound = 300,
         #endregion
 
+        #region OmdbService: 0400-0499
+        [Description("A keresett film nem található")]
+        OmdbServiceMovieNotFound = 400,
+        [Description("Nem sikerült lekérni a film adatait")]
+        OmdbServiceOmdbFetchFailed = 401,
+        #endregion
     }
 }
