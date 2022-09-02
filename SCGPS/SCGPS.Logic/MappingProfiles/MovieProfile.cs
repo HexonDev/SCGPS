3f18bf8a19b2e7ac4462ba219eddd958b50a34b5
Máté Stancz <stancz.mate@gmail.com>
2022-09-02 12:26:30 +0200

    movie mapping átalakítása

--- SCGPS/SCGPS.Logic/MappingProfiles/MovieProfile.cs
+++ SCGPS/SCGPS.Logic/MappingProfiles/MovieProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using SCGPS.Data.Entities;
+using SCGPS.Domain;
 using SCGPS.Domain.Dto;
 using SCGPS.Domain.Results;
 
@@ -9,7 +10,9 @@ namespace SCGPS.Logic.MappingProfiles
     {
         public MovieProfile()
         {
-            CreateMap<Movie, MovieDto>().ReverseMap();
+            CreateMap<Movie, MovieDto>()
+                .ForMember(dest => dest.MaxRating, options => options.MapFrom(source => source.ImdbRating.GetMaxRating()))
+                .ForMember(dest => dest.CurrentRating, options => options.MapFrom(source => source.ImdbRating.GetCurrentRating()));
             CreateMap<SimpleResult<Movie[]>, MoviesDto>()
                 .ForMember(dest => dest.Movies, options => options.MapFrom(source => source.Result));
         }
