80db9ec095c0b1cb50c34adb46a35c3e00fe2890
Máté Stancz <stancz.mate@gmail.com>
2022-09-02 12:42:42 +0200

    Error codeok fordítása

--- SCGPS/SCGPS.Domain/Enums/ErrorCodes.cs
+++ SCGPS/SCGPS.Domain/Enums/ErrorCodes.cs
@@ -10,18 +10,18 @@ namespace SCGPS.Domain.Enums
     public enum ErrorCodes
     {
         #region General: 0000-0099
-        [Description("Ismeretlen hiba")]
+        [Description("Unknown error")]
         UncategorizedError = 0000,
-        [Description("Validációs hiba")]
+        [Description("Validation error")]
         ValidationError = 0001,
         //[Description("")]
 
         #endregion
 
         #region Service (General): 0100-0199
-        [Description("A keresett adat nem található")]
+        [Description("The requested data couldn't be found")]
         ServiceGeneralEntityNotFound = 0100,
-        [Description("Az adott entitásnak nincs azonosítója")]
+        [Description("The entity has no ID")]
         ServiceGeneralNoId = 0101,
         #endregion
 
@@ -30,14 +30,14 @@ namespace SCGPS.Domain.Enums
         #endregion
 
         #region ReviewService: 0300-0399
-        [Description("Az értékelt film nem található")]
+        [Description("Reviewed movie not found")]
         ReviewServiceMovieNotFound = 300,
         #endregion
 
         #region OmdbService: 0400-0499
-        [Description("A keresett film nem található")]
+        [Description("Movie not found")]
         OmdbServiceMovieNotFound = 400,
-        [Description("Nem sikerült lekérni a film adatait")]
+        [Description("Can't fetch the movie's data")]
         OmdbServiceOmdbFetchFailed = 401,
         #endregion
     }
