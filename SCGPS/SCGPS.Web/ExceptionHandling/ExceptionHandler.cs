340e97aa50a3333540eb3c366552d9a5e44b9c7f
Máté Stancz <stancz.mate@gmail.com>
2022-09-02 12:27:43 +0200

    Exception handler middleware

---
+++ SCGPS/SCGPS.Web/ExceptionHandling/ExceptionHandler.cs
@@ -0,0 +1,50 @@
+﻿using SCGPS.Domain;
+using SCGPS.Domain.Dto;
+using SCGPS.Domain.Exceptions;
+using System.Net;
+using System.Net.Mime;
+using System.Text.Json;
+
+namespace SCGPS.Web.ExceptionHandling
+{
+	public class ExceptionHandler
+	{
+		private readonly RequestDelegate _next;
+
+		public ExceptionHandler(RequestDelegate next)
+		{
+			_next = next ?? throw new ArgumentNullException(nameof(next));
+		}
+
+		public async Task InvokeAsync(HttpContext httpContext)
+		{
+			try
+			{
+				await _next(httpContext);
+			}
+			catch (Exception ex)
+			{
+				var nEx = ex.GetException();
+				await HandleResponseAsync(httpContext, nEx);
+			}
+		}
+
+		private static async Task HandleResponseAsync(HttpContext context, ScGpsException exception)
+		{
+			context.Response.ContentType = MediaTypeNames.Application.Json;
+			context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+			await context.Response.WriteAsync(JsonSerializer.Serialize(new BaseDto
+			{
+				IsSucceded = false,
+				ErrorCode = $"{(int)exception.ErrorCode}",
+				ErrorDescription = exception.ErrorCode.GetErrorDescription(),
+				Time = DateTime.Now,
+			}, new JsonSerializerOptions()
+			{
+				PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+				WriteIndented = true,
+			}));
+		}
+	}
+}
--- SCGPS/SCGPS.Web/Program.cs
+++ SCGPS/SCGPS.Web/Program.cs
@@ -1,5 +1,6 @@
 using SCGPS.Data;
 using SCGPS.Logic;
+using SCGPS.Web.ExceptionHandling;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,15 @@ builder.Services.AddLogging();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(o =>
+{
+	o.AddPolicy(name: "scgpsCors", b =>
+	{
+		b.AllowAnyOrigin()
+		.AllowAnyHeader()
+		.AllowAnyMethod();
+	});
+});
 builder.Services.AddDataLayer(builder.Configuration);
 builder.Services.AddLogicLayer(builder.Configuration);
 
@@ -23,9 +33,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors("scgpsCors");
 app.UseAuthorization();
-
+app.UseMiddleware<ExceptionHandler>();
 app.MapControllers();
 
 app.Run();
