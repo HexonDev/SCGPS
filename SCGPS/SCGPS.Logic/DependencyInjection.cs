ea3b9a4e81e77c56c602c7872c0c5ac3ad8de259
Máté Stancz <stancz.mate@gmail.com>
2022-09-02 12:27:12 +0200

    Validátorok hozzáadása a DI-hoz

--- SCGPS/SCGPS.Logic/DependencyInjection.cs
+++ SCGPS/SCGPS.Logic/DependencyInjection.cs
@@ -2,12 +2,17 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SCGPS.Data.Entities;
+using SCGPS.Domain.Commands;
 using SCGPS.Domain.Commands.Entity;
 using SCGPS.Domain.Commands.MovieSvc;
+using SCGPS.Domain.Commands.OmdbSvc;
+using SCGPS.Domain.Commands.ReviewSvc;
 using SCGPS.Logic.Services.MovieSvc;
 using SCGPS.Logic.Services.OmdbSvc;
 using SCGPS.Logic.Services.ReviewSvc;
+using SCGPS.Logic.Validators;
 using SCGPS.Logic.Validators.MovieSvc;
+using SCGPS.Logic.Validators.OmdbSvc;
 using SCGPS.Logic.Validators.ReviewSvc;
 using System;
 using System.Collections.Generic;
@@ -27,8 +32,16 @@ namespace SCGPS.Logic
 
             #region Validátorok
             services.AddScoped<AbstractValidator<EntityCommand<Movie>>, MovieEntityCommandValidator>();
-            services.AddScoped<AbstractValidator<EntityCommand<Review>>, ReviewEntityCommandValidator>();
             services.AddScoped<AbstractValidator<MovieTitleCommand>, MovieTitleCommandValidator>();
+            services.AddScoped<AbstractValidator<GetMoviesCommand>, GetMoviesCommandValidator>();
+
+            services.AddScoped<AbstractValidator<GetOmdbMovieCommand>, GetOmdbMovieCommandValidator>();
+
+            services.AddScoped<AbstractValidator<EntityCommand<Review>>, ReviewEntityCommandValidator>();
+            services.AddScoped<AbstractValidator<AddReviewCommand>, AddReviewCommandValidator>();
+            services.AddScoped<AbstractValidator<GetReviewsCommand>, GetReviewsCommandValidator>();
+
+            services.AddScoped<AbstractValidator<IdCommand>, IdCommandValidator>();
             #endregion
 
             #region Servicek
